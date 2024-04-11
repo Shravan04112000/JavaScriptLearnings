c3e69fc16392954e6525c04c801f946097100ede
Shravan04112000 <121894687+Shravan04112000@users.noreply.github.com>
2024-04-11 22:10:24 +0530

    Add files via upload

---
+++ 02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict" // treat all js code as newer version
+
+// alert("hello") // we are using nodejs,not browser
+
+console.log(3+
+  
+  3); // code readability should be high
+
+console.log("Shravan")
+
+let name = "Shravan" // string
+
+let age = 90 // number
+
+let isLoggedIn = true
