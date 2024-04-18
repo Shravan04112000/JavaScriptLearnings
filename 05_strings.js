c9a15d605b9604a3ce56793f72a0ef7572c80a49
Shravan04112000 <121894687+Shravan04112000@users.noreply.github.com>
2024-04-18 21:22:58 +0530

    Add files via upload

---
+++ 05_strings.js
@@ -0,0 +1,24 @@
+const name = "Shravan";
+const repoCount = 50;
+const getName = new String('shravan');
+
+//console.log(name + repoCount + "Value")
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+// console.log(getName.length)
+// console.log(getName.toUpperCase())
+// console.log(getName.toLowerCase())
+
+console.log(getName.charAt(2))
+console.log(getName.indexOf('n'))
+
+const newString = getName.substring(0,4)
+console.log(newString)
+
+const anotherString = getName.slice(-6,4)
+console.log(anotherString)
+
+const newString1 = "                Shravan             "
+console.log(newString1)
+console.log(newString1.trim())
